a08a50740eb516f6b36fb43ed506d34dd1f18637
HasanShaikat <hasanshaikat9@gmail.com>
2022-04-13 22:07:53 +0600

    Js update

--- JS/money.js
+++ JS/money.js
@@ -21,31 +21,31 @@ document.getElementById('calculate').addEventListener('click', function (){
     const incomeInputValue = incomeInput.value;
     const incomeInputAmount = parseInt(incomeInputValue);
 
-    let food = getInput('foodInput');
-    
+    let food = getInput('foodInput');   
 
-    let rent = getInput('rentInput');
-    
+    let rent = getInput('rentInput');  
 
     let cloth = getInput('clothInput');
    
-
     // expenses and balance
     getExpenses('totalExpenses');
-
-    // let balance = getExpenses('balance').innerText;
-   
-    //total Expenses
-    let inputSum = food + rent + cloth;
-    totalExpenses.innerText = inputSum;
-
+  
+    // Balance
     const balance = document.getElementById('balance');
     const balanceText = balance.innerText;
     const balanceAmount = parseInt(balanceText);
 
-    // Balance
-    let totalAmount = incomeInputAmount - inputSum;
-    balance.innerText = totalAmount;
+    //total Expenses and Balance
+    if(food > 0 && rent > 0 && cloth > 0){
+        let inputSum = food + rent + cloth;
+        totalExpenses.innerText = inputSum;
+
+        let totalAmount = incomeInputAmount - inputSum;
+        balance.innerText = totalAmount;
+    }else{
+       alert("Please Input Correct Amount!!")
+    }
+    
 })
 
 function getSave(saving){
@@ -72,12 +72,15 @@ document.getElementById('saveButton').addEventListener('click', function (){
     const remainingBalanceAmount = parseFloat(remainingBalanceText);
 
     // saving balance
-    const percenteg = incomeInput.value / save;
-    savingAmount.innerText = percenteg;
+    if(save > 0){
+        const percenteg = (incomeInput.value / 100) * save;
+        savingAmount.innerText = percenteg;
 
-    const remainingTotalBalance = balance.innerText - savingAmount.innerText;
-    remainingBalance.innerText = remainingTotalBalance
-
+        const remainingTotalBalance = balance.innerText - savingAmount.innerText;
+        remainingBalance.innerText = remainingTotalBalance
+    }else{
+        alert("Please Give your Tax.")
+    }
 
     // income input clear
     incomeInput.value = '';
